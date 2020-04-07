9001278bfb167df701803ec09bc903a553bfde5c
Mimi <mimi@MacBook-Air-Oj-11.local>
2020-04-07 18:05:56 +0200

    backend double tested

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var usersRouter = require('./routes/users');
+var usersRouter = require("./routes/users");
+var servicesRouter = require("./routes/services");
 
 var app = express();
 
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", function(req, res, next) {
     res.send("I knew I could do this! BOOM!");
 });
 
-app.use('/cleanup', usersRouter);
+app.use("/cleanup", usersRouter);
+app.use("/cleanup", servicesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -30,11 +31,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
     // render the error page
     res.status(err.status || 500);
-    res.send('error');
+    res.send("error");
 });
 
 module.exports = app;
--- routes/services.js
+++ routes/services.js
@@ -5,88 +5,87 @@ const db = require("../model/helper");
 
 router.use(bodyParser.json());
 
-
 //API STARTS FROM HERE
 
 //TABLE 2:SERVICES
 //1.REQUEST FOR ALL INFO OF TABLE "USERS" IN DATABASE
-router.get('/services', (req, res) => {
+router.get("/services", (req, res) => {
     // Respond by send the full list of data in "services" table
-    db("SELECT * FROM users ORDER BY id ASC;")
-        .then(results => {
+    db("SELECT * FROM services ORDER BY id ASC;")
+        .then((results) => {
             res.send(results.data);
         })
-        .catch(err => res.status(500).send(err));
+        .catch((err) => res.status(500).send(err));
 });
 
 //2.QUERYING INFO ABOUT SPECIFIC COLUMN/ROW
-router.get('/services/:id', (req, res) => {
+router.get("/services/:id", (req, res) => {
     // Respond by send the full list of data in "users" table
-    db(`SELECT * FROM users WHERE id=${req.params.id};`)
-        .then(results => {
+    db(`SELECT * FROM services WHERE id=${req.params.id};`)
+        .then((results) => {
             res.send(results.data);
         })
-        .catch(err => res.status(500).send(err));
+        .catch((err) => res.status(500).send(err));
 });
 
 //3.CREATE A TABLE INTO THE DATABASE: USERS
-router.post('/services', (req, res) => {
+router.post("/services", (req, res) => {
     //launch a database query using mysql syntax:
     db(`INSERT INTO services (type, name, description,
-        availability, location, userId) VALUES 
-('${req.body.type}', '${req.body.name}', '${req.body.description}, '${req.body.availability}', '${req.body.location}', ${req.body.userId});`)
-        .then(results => {
+        availability, location, userId) VALUES
+('${req.body.type}', '${req.body.name}', '${req.body.description}', '${req.body.availability}', '${req.body.location}', ${req.body.userId});`)
+        .then((results) => {
             if (results.error) {
                 res.status("ERROR! TRY AGAIN").send({
-                    error: results.error
+                    error: results.error,
                 });
             } else {
                 res.send({
-                    body: results.data
+                    body: results.data,
                 });
             }
         })
-        .catch(err => res.status("NAH,TRY AGAIN").send(err));
+        .catch((err) => res.status("NAH,TRY AGAIN").send(err));
 });
 
 //4.UPDATE DATA IN THE DATABASE
-router.put('/services/:id', (req, res) => {
+router.put("/services/:id", (req, res) => {
     // The request's body is available in req.body
     // URL params are available in req.params
-    db(`UPDATE services set name=
-            ('${req.body.type}', '${req.body.name}', '${req.body.description}', '${req.body.availability}', '${req.body.location}', ${req.body.userId} WHERE id=${req.params.id};`)
-        .then(results => {
+    db(`UPDATE services set type =
+            '${req.body.type}', name ='${req.body.name}', description ='${req.body.description}', availability ='${req.body.availability}', location = '${req.body.location}', userId =${req.body.userId} WHERE id = ${req.params.id};`)
+        .then((results) => {
             if (results.error) {
                 res.status("ERROR! TRY AGAIN").send({
                     //OR  //res.status(404).send({
-                    error: results.error
+                    error: results.error,
                 });
             } else {
                 res.send({
-                    body: results.data
+                    body: results.data,
                 });
             }
         })
-        .catch(err => res.status("NAH,TRY AGAIN").send(err));
+        .catch((err) => res.status("NAH,TRY AGAIN").send(err));
 });
 
 //5.DELETE
-router.delete('/services/:id', (req, res) => {
+router.delete("/services/:id", (req, res) => {
     // URL params are available in req.params
     db(`DELETE FROM services WHERE id=${req.params.id};`)
-        .then(results => {
+        .then((results) => {
             if (results.error) {
                 res.status("ERROR! TRY AGAIN").send({
                     //OR res.status(404).send({
-                    error: results.error
+                    error: results.error,
                 });
             } else {
                 res.send({
-                    body: results.data
+                    body: results.data,
                 });
             }
         })
-        .catch(err => res.status("NAH,TRY AGAIN").send(err));
+        .catch((err) => res.status("NAH,TRY AGAIN").send(err));
 });
 
 module.exports = router;
